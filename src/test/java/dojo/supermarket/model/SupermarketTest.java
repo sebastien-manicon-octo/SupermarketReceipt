adf6fa43a526210014ec10ffea21f59ba06dd3d4
Sébastien MANICON <sebastien.manicon@octo.com>
2026-04-22 18:30:14 +0200

    Create the golden master test

--- src/test/java/dojo/supermarket/model/SupermarketTest.java
+++ src/test/java/dojo/supermarket/model/SupermarketTest.java
@@ -4,7 +4,7 @@ import dojo.supermarket.ReceiptPrinter;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,7 +25,7 @@ class SupermarketTest {
 
         ShoppingCart cart = new ShoppingCart();
         cart.addItemQuantity(apples, 2.5);
-        
+
         // ACT
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
@@ -36,10 +36,73 @@ class SupermarketTest {
         ReceiptItem receiptItem = receipt.getItems().get(0);
         assertEquals(apples, receiptItem.getProduct());
         assertEquals(1.99, receiptItem.getPrice());
-        assertEquals(2.5*1.99, receiptItem.getTotalPrice());
+        assertEquals(2.5 * 1.99, receiptItem.getTotalPrice());
         assertEquals(2.5, receiptItem.getQuantity());
 
     }
 
+    private final List<Product> PRODUCTS = Arrays.asList(
+            new Product("toothbrush", ProductUnit.EACH),
+            new Product("soap", ProductUnit.EACH),
+            new Product("pasta", ProductUnit.EACH),
+            new Product("apples", ProductUnit.KILO),
+            new Product("peach", ProductUnit.KILO),
+            new Product("kiwi", ProductUnit.KILO)
+    );
 
+    @Test
+    public void golden_master() {
+        Random random = new Random(42);
+
+        StringBuilder sb = new StringBuilder();
+        for(int x = 0; x < 100; x++) {
+            SupermarketCatalog catalog = new FakeCatalog();
+            Set<Product> existingProducts = new HashSet<>();
+            for (int i = 0; i < random.nextInt(10) + 1; i++) {
+                Product product = PRODUCTS.get(random.nextInt(PRODUCTS.size()));
+                existingProducts.add(product);
+                catalog.addProduct(
+                        product,
+                        random.nextDouble()
+                );
+            }
+
+            Teller teller = new Teller(catalog);
+            for (int i = 0; i < random.nextInt(10) + 1; i++) {
+                SpecialOfferType[] values = SpecialOfferType.values();
+                Product product = PRODUCTS.get(random.nextInt(PRODUCTS.size()));
+
+                if (!existingProducts.contains(product)) {
+                    continue;
+                }
+
+                teller.addSpecialOffer(
+                        values[random.nextInt(values.length)],
+                        product,
+                        random.nextDouble() % 5
+                );
+            }
+
+            ShoppingCart cart = new ShoppingCart();
+            for (int i = 0; i < random.nextInt(10) + 1; i++) {
+                Product product = PRODUCTS.get(random.nextInt(PRODUCTS.size()));
+
+                if (!existingProducts.contains(product)) {
+                    continue;
+                }
+
+                cart.addItemQuantity(
+                        product,
+                        random.nextDouble() * 5
+                );
+            }
+
+            Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+            ReceiptPrinter printer = new ReceiptPrinter();
+            sb.append(printer.printReceipt(receipt));
+        }
+
+        Approvals.verify(sb.toString());
+    }
 }
