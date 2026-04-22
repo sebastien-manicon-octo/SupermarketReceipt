9cac8f2c611dd4467afd1e9e9fd74ee9074adc8e
Sébastien MANICON <sebastien.manicon@octo.com>
2026-04-22 20:40:19 +0200

    split discount applying

--- src/main/java/dojo/supermarket/model/Offer.java
+++ src/main/java/dojo/supermarket/model/Offer.java
@@ -12,6 +12,21 @@ public class Offer {
         this.product = product;
     }
 
+    Discount getDiscount(double unitPrice, double quantity) {
+        switch (offerType) {
+            case TWO_FOR_AMOUNT:
+                return SpecialOfferType.twoforamount(this, product, unitPrice, quantity);
+            case THREE_FOR_TWO:
+                return SpecialOfferType.threeForTwo(this, product, unitPrice, quantity);
+            case TEN_PERCENT_DISCOUNT:
+                return SpecialOfferType.tenPercent(this, product, unitPrice, quantity);
+            case FIVE_FOR_AMOUNT:
+                return SpecialOfferType.fiveForAmount(this, product, unitPrice, quantity);
+            default:
+                return null;
+        }
+    }
+
     Product getProduct() {
         return product;
     }
--- src/main/java/dojo/supermarket/model/SpecialOfferType.java
+++ src/main/java/dojo/supermarket/model/SpecialOfferType.java
@@ -5,4 +5,45 @@ public enum SpecialOfferType {
     TEN_PERCENT_DISCOUNT,
     TWO_FOR_AMOUNT,
     FIVE_FOR_AMOUNT,
+    ;
+
+    static Discount fiveForAmount(Offer offer, Product p, double unitPrice, double quantity) {
+        int x = 5;
+        int quantityAsInt = (int) quantity;
+        if (quantityAsInt < 5) {
+            return null;
+        }
+
+        int numberOfXs = quantityAsInt / x;
+        double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+        return new Discount(p, x + " for " + offer.argument, -discountTotal);
+    }
+
+    static Discount tenPercent(Offer offer, Product p, double unitPrice, double quantity) {
+        return new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
+    }
+
+    static Discount threeForTwo(Offer offer, Product p, double unitPrice, double quantity) {
+        int x = 3;
+        int quantityAsInt = (int) quantity;
+        if (quantityAsInt <= 2) {
+            return null;
+        }
+
+        int numberOfXs = quantityAsInt / x;
+        double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+        return new Discount(p, "3 for 2", -discountAmount);
+    }
+
+    static Discount twoforamount(Offer offer, Product p, double unitPrice, double quantity) {
+        int x = 2;
+        int quantityAsInt = (int) quantity;
+        if (quantityAsInt < 2) {
+            return null;
+        }
+        
+        double total = offer.argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
+        double discountN = unitPrice * quantity - total;
+        return new Discount(p, "2 for " + offer.argument, -discountN);
+    }
 }
